b32324d94b755571544b9cb4b23d99f5a9b175b9
paryi <103213688+paryi555@users.noreply.github.com>
2024-11-09 14:35:22 +0800

    Add files via upload

--- configs.py
+++ configs.py
@@ -15,11 +15,11 @@ def pwclonet_args():
     parser.add_argument('--limit_or_filter', type=bool, default=True, help='if False, filter will reserve 40m~50m points')
     parser.add_argument('--batch_size', type=int, default=8, help='Batch Size during training [default: 16]')
     parser.add_argument('--eval_batch_size', type=int, default=1, help='Batch Size during evaling [default: 64]')
-    parser.add_argument('--eval_before', type=int, default=0, help='if 1, eval before train')
+    parser.add_argument('--eval_before', type=int, default=1, help='if 1, eval before train')
 
     # parser.add_argument('--data_root', default='/tmp/data_odometry_velodyne/dataset', help='Dataset directory [default: /dataset]')
     parser.add_argument('--lidar_root', default='/dataset/data_odometry_velodyne/dataset/', help='Dataset directory [default: /dataset]')
-    parser.add_argument('--image_root', default='/dataset/data_odometry_color/', help='Dataset directory [default: /dataset]')
+    parser.add_argument('--image_root', default='/dataset/data_odometry_color/sequences', help='Dataset directory [default: /dataset]')
     parser.add_argument('--log_dir', default='log_train', help='Log dir [default: log_train]')
 
     parser.add_argument('--num_points', type=int, default=150000, help='Point Number [default: 2048]')
@@ -27,11 +27,11 @@ def pwclonet_args():
     parser.add_argument('--H_input', type=int, default=64, help='H Number [default: 64]')
     parser.add_argument('--W_input', type=int, default=1800, help='W Number [default: 1800]')
 
-    parser.add_argument('--max_epoch', type=int, default=501, help='Epoch to run [default: 151]')
+    parser.add_argument('--max_epoch', type=int, default=301, help='Epoch to run [default: 151]')
     parser.add_argument('--weight_decay', type=int, default=0.0001, help='The Weight decay [default : 0.0001]')
     parser.add_argument('--workers', type=int, default=6,
                         help='Sets how many child processes can be used [default : 16]')
-    parser.add_argument('--model_name', type=str, default='pwclonet', help='base_dir_name [default: pwclonet]')
+    parser.add_argument('--model_name', type=str, default='DVLO', help='base_dir_name [default: DVLO]')
     parser.add_argument('--task_name', type=str, default=None, help='who can replace model_name ')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
 
